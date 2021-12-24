70445b7e4c62b5359c01f19fa2a1ef3b787622a6
Nilanjan1998 <81284658+Nilanjan1998@users.noreply.github.com>
2021-12-24 10:25:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+
+        function updateTextInput(val) {
+          document.getElementById('textInput').innerHTML=val+'%'; 
+        }
+
+function compute(){
+	var amount=document.getElementById('principal').value;
+	var interest=parseFloat(document.getElementById('range').value);
+	var years=parseInt(document.getElementById('years').value);
+	var totalAmount=(amount*interest*years)/100;
+	var currentYear=new Date().getFullYear()+years;
+	document.getElementById('finalValue').innerHTML='If you deposit '+amount
++',<br>'+
+	'at an interest rate of '+interest+'%.<br>'+'You will receive an amount of '+totalAmount+',<br>'
+	+'in the year '+currentYear;
 }
-        
